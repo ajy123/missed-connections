be7092221deb47f71cb4388222671b7e51cd0eba
joanna <25386792+ajy123@users.noreply.github.com>
2024-07-07 10:28:56 -0400

    fix step margin and table highlight

--- bee.js
+++ bee.js
@@ -1,118 +1,143 @@
-// // testing d3 code
-// // let lastDate;
+// testing d3 code
+// let lastDate;
 // let data; // Declare a global variable
 // let dateRange;
-// // test the code
-// let height = 400;
-// let width = 1000;
-// let margin = { top: 0, right: 40, bottom: 30, left: 40 };
+// test the code
+let height = 320;
+let width = 640; //window.innerWidth;
+let margin = { top: 20, right: 40, bottom: 30, left: 40 };
+// console.log(window.innerWidth);
+// // test out broough color
 
-// // // test out broough color
-// // let colors = d3
-// //   .scaleOrdinal()
-// //   .domain([
-// //     "Manhattan",
-// //     "Brooklyn",
-// //     "The Bronx",
-// //     "Staten Island",
-// //     "Queens",
-// //   ])
-// //   .range(["#D81B60", "#1976D2", "#388E3C", "#FBC02D", "#E64A19"]);
+function getData() {
+  return d3
+    .tsv("data/mc_final_w_emojis.csv")
+    .then(function (d) {
+      // Here we can process data
+      data = d; // Assign the data to the global variable
+      return d;
+    })
+    .catch(function (error) {
+      // Handle error...
+      console.error(error);
+    });
+}
 
-// function getData() {
-//   return d3
-//     .tsv("data/mc_final_w_emojis.csv")
-//     .then(function (d) {
-//       // Here we can process data
-//       data = d; // Assign the data to the global variable
-//       return d;
-//     })
-//     .catch(function (error) {
-//       // Handle error...
-//       console.error(error);
-//     });
-// }
+getData().then(() => {
+  console.log(data); // Now globalData is populated with the data
+  dateRange = d3.extent(data, (d) => new Date(d.date));
 
-// getData().then(() => {
-//   console.log(data); // Now globalData is populated with the data
-//   dateRange = d3.extent(data, (d) => new Date(d.date));
+  let svg = d3
+    .select(".bee-emoji-chart")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-//   let svg = d3
-//     .select(".testd3")
-//     .append("svg")
-//     .attr("width", width)
-//     .attr("height", height);
+  let xScale = d3
+    .scaleUtc()
+    .domain(dateRange)
+    .range([margin.left, width - margin.right]);
 
-//   let xScale = d3
-//     .scaleUtc()
-//     .domain(dateRange)
-//     .range([margin.left, width - margin.right]);
+  //   svg
+  //     .append("g")
+  //     .attr("transform", `translate(0,${height - margin.bottom})`)
+  //     .call(
+  //       d3
+  //         .axisBottom(xScale)
+  //         .ticks(width / 60)
+  //         .tickSizeOuter(0)
+  //     );
 
-//   svg
-//     .append("g")
-//     .attr("transform", `translate(0,${height - margin.bottom})`)
-//     .call(
-//       d3
-//         .axisBottom(xScale)
-//         .ticks(width / 60)
-//         .tickSizeOuter(0)
-//     );
+  let yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data.map((d) => d.post.length)))
+    .range([height - margin.bottom, margin.top]);
 
-//   let yScale = d3
-//     .scaleLinear()
-//     .domain(d3.extent(data.map((d) => d.post.length)))
-//     .range([height - margin.bottom, margin.top]);
+  let colors = d3
+    .scaleOrdinal()
+    .domain(["Manhattan", "Brooklyn", "Queens", "The Bronx", "Staten Island"])
+    .range(["#0081A7", "#00AFB9", "#FFED4B", "#F4B54E", "#FE8478"]);
 
-//   let color = d3
-//     .scaleOrdinal()
-//     .domain(d3.extent(data, (d) => d.borough))
-//     .range(d3.schemePaired);
+  const circles = svg
+    .selectAll(".circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("class", "circle")
+    .attr("stroke", (d) => colors(d.borough))
+    .attr("fill", (d) => colors(d.borough))
+    .attr("opacity", 0.5)
+    .attr("r", 10)
+    .attr("cx", (d) => xScale(new Date(d.date)))
+    .attr("cy", (d) => yScale(d.post.length));
+  const emojis = svg
+    .selectAll(".emoji")
+    .data(data)
+    .enter()
+    .append("text")
+    .attr("class", "emoji")
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "middle")
+    .attr("font-size", "12px") // Adjust the font size as needed
+    .attr("x", (d) => xScale(new Date(d.date)))
+    .attr("y", (d) => yScale(d.post.length))
+    .text((d) => {
+      let emojis = JSON.parse(d.cleaned_emojis.replace(/'/g, '"'));
+      return emojis.length
+        ? emojis[Math.floor(Math.random() * emojis.length)]
+        : "";
+    });
 
-//   console.log(data[0].cleaned_emojis[2]);
+  // svg
+  //   .selectAll(".circ")
+  //   .data(data)
+  //   .enter()
+  //   .append("circle")
+  //   .attr("class", "circ")
+  //   // .attr("r", (d) => size(d["Market Cap"]))
+  //   .attr("fill", (d) => color(d.borough))
+  //   .attr("r", 5)
+  //   .attr("cx", (d) => xScale(new Date(d.date)))
+  //   .attr("cy", (d) => yScale(d.post.length))
+  //   .append("text")
+  //   .attr("class", "emoji")
+  //   .attr("text-anchor", "middle")
+  //   .attr("dy", ".35em")
+  //   .text((d) => {
+  //     // console.log();
+  //     // let emojis = JSON.parse(d.cleaned_emojis[0]);
+  //     // return emojis.length
+  //     //   ? emojis[Math.floor(Math.random() * emojis.length)]
+  //     //   : "";
 
-//   svg
-//     .selectAll(".emoji")
-//     .data(data)
-//     .enter()
-//     .append("text")
-//     .attr("class", "emoji")
-//     .attr("text-anchor", "middle")
-//     .attr("font-size", "12px") // Adjust the font size as needed
-//     .attr("x", (d) => xScale(new Date(d.date)))
-//     .attr("y", (d) => yScale(d.post.length))
-//     .text((d) => {
-//       let emojis = JSON.parse(d.cleaned_emojis.replace(/'/g, '"'));
-//       return emojis.length
-//         ? emojis[Math.floor(Math.random() * emojis.length)]
-//         : "";
-//     });
+  //   });
 
-//   // svg
-//   //   .selectAll(".circ")
-//   //   .data(data)
-//   //   .enter()
-//   //   .append("circle")
-//   //   .attr("class", "circ")
-//   //   // .attr("r", (d) => size(d["Market Cap"]))
-//   //   .attr("fill", (d) => color(d.borough))
-//   //   .attr("r", 5)
-//   //   .attr("cx", (d) => xScale(new Date(d.date)))
-//   //   .attr("cy", (d) => yScale(d.post.length))
-//   //   .append("text")
-//   //   .attr("class", "emoji")
-//   //   .attr("text-anchor", "middle")
-//   //   .attr("dy", ".35em")
-//   //   .text((d) => {
-//   //     // console.log();
-//   //     // let emojis = JSON.parse(d.cleaned_emojis[0]);
-//   //     // return emojis.length
-//   //     //   ? emojis[Math.floor(Math.random() * emojis.length)]
-//   //     //   : "";
+  window.addEventListener("scroll", () => {
+    const scrollY = window.scrollY;
+    // console.log(scrollY);
+    circles.each(function (d) {
+      const initialY = yScale(d.post.length);
+      const newY = initialY - scrollY * 0.5; // Adjust the factor as needed
+      const newOpacity = 0.5 - scrollY / 500; // Adjust the factor as needed
 
-//   //   });
+      d3.select(this)
+        .style("transform", `translateY(${newY - initialY}px)`)
+        .style("opacity", newOpacity > 0 ? newOpacity : 0);
+    });
 
-//   // Additional debug information
-//   console.log("dateRange:", dateRange);
-//   console.log("xScale domain:", yScale.domain());
-//   console.log("xScale range:", yScale.range());
-// });
+    emojis.each(function (d) {
+      const initialY = yScale(d.post.length);
+      const newY = initialY - scrollY * 0.5; // Adjust the factor as needed
+      const newOpacity = 1 - scrollY / 500; // Adjust the factor as needed
+
+      d3.select(this)
+        .style("transform", `translateY(${newY - initialY}px)`)
+        .style("opacity", newOpacity > 0 ? newOpacity : 0);
+    });
+  });
+
+  // Additional debug information
+  console.log("dateRange:", dateRange);
+  console.log("xScale domain:", yScale.domain());
+  console.log("xScale range:", yScale.range());
+});
--- main.js
+++ main.js
@@ -8,6 +8,11 @@ function hideNote() {
   d3.select("#statenisland").style("opacity", 0.5);
 }
 
+function removeHighlight() {
+  console.log("remove highlight");
+  d3.selectAll("td").classed("highlight", false);
+}
+
 /* Scrollytelling code goes under here */
 hideNote();
 
@@ -25,15 +30,19 @@ d3.select("#step-one").on("stepin", function (e) {
   // undo changes from step two?
 
   // // do changes for step one
-  d3.select("#brooklyn").classed("hidden", true);
+  // d3.select("#brooklyn").classed("hidden", true);
+  d3.select("#brooklyn").style("opacity", 0.2);
 
   d3.select("#manhattan").classed("hidden", false);
   d3.select("#manhattan").style("opacity", 1);
   d3.select("#longestPost").style("opacity", 1);
 
-  d3.select("#queens").classed("hidden", true);
-  d3.select("#bronx").classed("hidden", true);
-  d3.select("#statenisland").classed("hidden", true);
+  // d3.select("#queens").classed("hidden", true);
+  d3.select("#queens").style("opacity", 0.2);
+  // d3.select("#bronx").classed("hidden", true);
+  d3.select("#bronx").style("opacity", 0.2);
+  d3.select("#statenisland").style("opacity", 0.2);
+  // d3.select("#statenisland").classed("hidden", true);
 });
 
 d3.select("#step-two").on("stepin", function (e) {
@@ -41,55 +50,73 @@ d3.select("#step-two").on("stepin", function (e) {
 
   // do changes for step two
   console.log("Got to step two");
-  d3.select("#manhattan").classed("hidden", true);
-  d3.select("#longestPost").classed("hidden", true);
+  // d3.select("#manhattan").classed("hidden", true);
+  // d3.select("#longestPost").classed("hidden", true);
+  d3.select("#manhattan").style("opacity", 0.2);
+  d3.select("#longestPost").style("opacity", 0.2);
 
   d3.select("#brooklyn").classed("hidden", false);
   d3.select("#brooklyn").style("opacity", 1);
 
-  d3.select("#queens").classed("hidden", true);
-  d3.select("#bronx").classed("hidden", true);
-  d3.select("#statenisland").classed("hidden", true);
-  // d3.selectAll("[data-name='africa'] path").style("stroke", "black");
+  // d3.select("#queens").classed("hidden", true);
+  d3.select("#queens").style("opacity", 0.2);
+  // d3.select("#bronx").classed("hidden", true);
+  d3.select("#bronx").style("opacity", 0.2);
+  // d3.select("#statenisland").classed("hidden", true);
+  d3.select("#statenisland").style("opacity", 0.2);
 });
 
 d3.select("#step-three").on("stepin", function (e) {
   // undo changes from step two
-  d3.select("#manhattan").classed("hidden", true);
-  d3.select("#longestPost").classed("hidden", true);
+  // d3.select("#manhattan").classed("hidden", true);
+  // d3.select("#longestPost").classed("hidden", true);
+  d3.select("#manhattan").style("opacity", 0.2);
+  d3.select("#longestPost").style("opacity", 0.2);
 
-  d3.select("#brooklyn").classed("hidden", true);
+  // d3.select("#brooklyn").classed("hidden", true);
+  d3.select("#brooklyn").style("opacity", 0.2);
 
   d3.select("#queens").classed("hidden", false);
   d3.select("#queens").style("opacity", 1);
 
-  d3.select("#bronx").classed("hidden", true);
-  d3.select("#statenisland").classed("hidden", true);
+  // d3.select("#bronx").classed("hidden", true);
+  d3.select("#bronx").style("opacity", 0.2);
+  // d3.select("#statenisland").classed("hidden", true);
+  d3.select("#statenisland").style("opacity", 0.2);
 });
 
 d3.select("#step-four").on("stepin", function (e) {
-  // undo changes from step two
-  d3.select("#manhattan").classed("hidden", true);
-  d3.select("#longestPost").classed("hidden", true);
+  // d3.select("#manhattan").classed("hidden", true);
+  // d3.select("#longestPost").classed("hidden", true);
+  d3.select("#manhattan").style("opacity", 0.2);
+  d3.select("#longestPost").style("opacity", 0.2);
 
-  d3.select("#brooklyn").classed("hidden", true);
+  // d3.select("#brooklyn").classed("hidden", true);
+  d3.select("#brooklyn").style("opacity", 0.2);
 
-  d3.select("#queens").classed("hidden", true);
+  // d3.select("#queens").classed("hidden", true);
+  d3.select("#queens").style("opacity", 0.2);
 
   d3.select("#bronx").classed("hidden", false);
   d3.select("#bronx").style("opacity", 1);
 
-  d3.select("#statenisland").classed("hidden", true);
+  // d3.select("#statenisland").classed("hidden", true);
+  d3.select("#statenisland").style("opacity", 0.2);
 });
 
 d3.select("#step-five").on("stepin", function (e) {
   // undo changes from step two
-  d3.select("#manhattan").classed("hidden", true);
-  d3.select("#longestPost").classed("hidden", true);
+  // d3.select("#manhattan").classed("hidden", true);
+  // d3.select("#longestPost").classed("hidden", true);
+  d3.select("#manhattan").style("opacity", 0.2);
+  d3.select("#longestPost").style("opacity", 0.2);
 
-  d3.select("#brooklyn").classed("hidden", true);
-  d3.select("#queens").classed("hidden", true);
-  d3.select("#bronx").classed("hidden", true);
+  // d3.select("#brooklyn").classed("hidden", true);
+  d3.select("#brooklyn").style("opacity", 0.2);
+
+  // d3.select("#queens").classed("hidden", true);
+  d3.select("#queens").style("opacity", 0.2);
+  d3.select("#bronx").style("opacity", 0.2);
 
   d3.select("#statenisland").classed("hidden", false);
   d3.select("#statenisland").style("opacity", 1);
@@ -104,24 +131,22 @@ d3.select("#noun-step-one").on("stepout", function (e) {
   }
 });
 
-function removeHighlight() {
-  console.log("remove highlight");
-  d3.selectAll("td").classed("highlight", false);
-}
-
 d3.select("#noun-step-one").on("stepin", function (e) {
   // highlight all the similarity across five boroughs
   console.log("This is noun step1");
   d3.selectAll("td").each(function (d) {
     if (
-      d &&
-      ((d.column === "Manhattan" && d.value === "time") ||
-        (d.column === "Brooklyn" && d.value === "time") ||
-        (d.column === "Queens" && d.value === "time") ||
-        (d.column === "Bronx" && d.value === "morning") ||
-        (d.column === "Staten Island" && d.value === "time"))
+      (d.column === "Manhattan" && d.value === "time") ||
+      (d.column === "Brooklyn" && d.value === "time") ||
+      (d.column === "Queens" && d.value === "time") ||
+      // (d.column === "Bronx" && d.value === "morning") ||
+      (d.column === "Staten Island" && d.value === "time")
     ) {
-      d3.select(this).classed("highlight", true);
+      d3.select(this).classed("highlight", true).text(`⌚ time`);
+    }
+
+    if (d.column === "Bronx" && d.value === "morning") {
+      d3.select(this).classed("highlight", true).text(`☀️ morning`);
     }
   });
 });
@@ -134,15 +159,32 @@ d3.select("#noun-step-two").on("stepin", function (e) {
   console.log("This is noun step2");
   d3.selectAll("td").each(function (d) {
     if (
-      d &&
-      ((d.column === "Manhattan" && d.value === "train") ||
-        (d.column === "Manhattan" && d.value === "eye") ||
-        (d.column === "Brooklyn" && d.value === "train") ||
-        (d.column === "Brooklyn" && d.value === "eye") ||
-        (d.column === "Queens" && d.value === "train") ||
-        (d.column === "Queens" && d.value === "eye"))
+      (d.column === "Manhattan" && d.value === "time") ||
+      (d.column === "Brooklyn" && d.value === "time") ||
+      (d.column === "Queens" && d.value === "time") ||
+      (d.column === "Staten Island" && d.value === "time")
     ) {
-      d3.select(this).classed("highlight", true);
+      d3.select(this).classed("highlight", false).text(`time`);
+    }
+
+    if (d.column === "Bronx" && d.value === "morning") {
+      d3.select(this).classed("highlight", false).text(`morning`);
+    }
+
+    if (
+      (d.column === "Manhattan" && d.value === "train") ||
+      (d.column === "Brooklyn" && d.value === "train") ||
+      (d.column === "Queens" && d.value === "train")
+    ) {
+      d3.select(this).classed("highlight", true).text(`🚆 train`);
+    }
+
+    if (
+      (d.column === "Manhattan" && d.value === "eye") ||
+      (d.column === "Brooklyn" && d.value === "eye") ||
+      (d.column === "Queens" && d.value === "eye")
+    ) {
+      d3.select(this).classed("highlight", true).text(`👀 eye`);
     }
   });
 });
@@ -154,8 +196,25 @@ d3.select("#noun-step-three").on("stepin", function (e) {
   // do changes for step two
   console.log("This is noun step2");
   d3.selectAll("td").each(function (d) {
+    if (
+      d &&
+      ((d.column === "Manhattan" && d.value === "train") ||
+        (d.column === "Brooklyn" && d.value === "train") ||
+        (d.column === "Queens" && d.value === "train"))
+    ) {
+      d3.select(this).classed("highlight", false).text(`train`);
+    }
+
+    if (
+      (d.column === "Manhattan" && d.value === "eye") ||
+      (d.column === "Brooklyn" && d.value === "eye") ||
+      (d.column === "Queens" && d.value === "eye")
+    ) {
+      d3.select(this).classed("highlight", false).text(`eye`);
+    }
+
     if (d && d.column === "Queens" && d.value === "smile") {
-      d3.select(this).classed("highlight", true);
+      d3.select(this).classed("highlight", true).text(`😊 smile`);
     }
   });
 });
@@ -167,8 +226,13 @@ d3.select("#noun-step-four").on("stepin", function (e) {
   // do changes for step two
   console.log("This is noun step2");
   d3.selectAll("td").each(function (d) {
+    // remove previous step class
+    if (d && d.column === "Queens" && d.value === "smile") {
+      d3.select(this).classed("highlight", false).text(`smile`);
+    }
+
     if (d && d.column === "Bronx" && d.value === "nail") {
-      d3.select(this).classed("highlight", true);
+      d3.select(this).classed("highlight", true).text(`💅 nail`);
     }
   });
 });
@@ -176,15 +240,17 @@ d3.select("#noun-step-four").on("stepin", function (e) {
 d3.select("#noun-step-five").on("stepin", function (e) {
   // // undo changes from step one
   removeHighlight();
-
-  // do changes for step two
-  console.log("This is noun step2");
   d3.selectAll("td").each(function (d) {
-    if (
-      (d && d.column === "Staten Island" && d.value === "bike") ||
-      (d && d.column === "Staten Island" && d.value === "beard")
-    ) {
-      d3.select(this).classed("highlight", true);
+    if (d && d.column === "Bronx" && d.value === "nail") {
+      d3.select(this).classed("highlight", false).text(`nail`);
+    }
+
+    if (d && d.column === "Staten Island" && d.value === "bike") {
+      d3.select(this).classed("highlight", true).text(`🚲 bike`);
+    }
+
+    if (d && d.column === "Staten Island" && d.value === "beard") {
+      d3.select(this).classed("highlight", true).text(`🧔‍♂️ bike`);
     }
   });
 });
